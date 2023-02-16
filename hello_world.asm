2ad86bfbf784bace1778a7e7be6d30d00a5508b5
Mitchell Foote <mcfoote321@gmail.com>
2023-02-16 18:57:59 -0500

    set up vars

--- hello_world.asm
+++ hello_world.asm
@@ -1,1 +1,11 @@
 ; hello_world.asm
+;
+; Author: Mitchell Foote
+
+
+section .text:
+    mov eax, 0x4        ; use write system call
+
+section .data:
+    message: db "Hellow World!!", 0xA
+    message_length equ $-message
