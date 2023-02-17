4d8e578c0bd47b52cf10871f81422da170434c8c
Mitchell Foote <mcfoote321@gmail.com>
2023-02-16 19:05:01 -0500

    work on body

--- hello_world.asm
+++ hello_world.asm
@@ -2,9 +2,19 @@
 ;
 ; Author: Mitchell Foote
 
+global _start
 
 section .text:
-    mov eax, 0x4        ; use write system call
+
+_start:
+    mov eax, 0x4            ; use write system call
+    mov ecx, 1              ; use stdout for fd
+    mov ecx, message        ;
+    mov edx, message_length ;
+    int 0x80                ; interupt syscall
+    mov eax, 0x1            ;
+    mov ebx, 0              ;
+    int 0x80                ;
 
 section .data:
     message: db "Hellow World!!", 0xA
